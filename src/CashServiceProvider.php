5ec7632a849fde728336df4226150598145ade22
Enas <enasasmar882@gmail.com>
2023-02-22 11:56:02 +0200

    fixes service provider

--- src/CashServiceProvider.php
+++ src/CashServiceProvider.php
@@ -2,21 +2,27 @@
 
 namespace Corals\Modules\Payment\Cash;
 
-use Illuminate\Support\ServiceProvider;
+use Corals\Foundation\Providers\BasePackageServiceProvider;
 use Corals\Settings\Facades\Modules;
 
-class CashServiceProvider extends ServiceProvider
+class CashServiceProvider extends BasePackageServiceProvider
 {
+    /**
+     * @var
+     */
     protected $defer = false;
+    /**
+     * @var
+     */
+    protected $packageCode = 'corals-payment-cash';
 
     /**
      * Bootstrap the application events.
      *
      * @return void
      */
-    public function boot()
+    public function bootPackage()
     {
-        $this->registerModulesPackages();
     }
 
     /**
@@ -24,7 +30,7 @@ class CashServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function registerPackage()
     {
     }
 
